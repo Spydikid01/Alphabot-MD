ad407e86ba6314faaccf00c64d618d3070da3d38
Spydikid01 <122142744+Spydikid01@users.noreply.github.com>
2023-04-16 11:34:05 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,12 +3,12 @@ const chalk = require('chalk')
 
 // Website Api
 global.APIs = {
-	alfa: 'https://api-alphabot.herokuapp.com',
+	alfa: 'spydikid894@gmail.com',
 }
 
 // Free apikey
 global.APIKeys = {
-	'https://api-alphabot.herokuapp.com': 'Alphabot',
+	'https://api-alphabot.herokuapp.com': '𝘙𝘖𝘓𝘌𝘟-𝘔𝘋',
 }
 
 // setting 
@@ -18,19 +18,19 @@ global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ x Alien-Alfa"
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
-global.myweb ="https://api-alphabot.herokuapp.com/"
-global.youtube = "https://youtube.com/c/alienalfa"
-global.github = "https://zeeoneofc.github.io/"
-global.ownernomer = "62887435047326"
-global.ownernomerr = "+62887435047326"
+global.botname = "𝘙𝘖𝘓𝘌𝘟-𝘔𝘋^ x 𝑆𝛲𝑌𝐷𝛪_.𝛫𝛪𝐷"
+global.ownername= "ᴹᴿ᭄𝑆𝛲𝑌𝐷𝛪_.𝛫𝛪𝐷×፝֟͜×"
+global.myweb ="spydikid894@gmail.com"
+global.youtube = "https://youtube.com/@hackergamingyt7597"
+global.github = "https://Spydikid01.github.io/"
+global.ownernomer = "919947946045"
+global.ownernomerr = "+919947946045"
 global.thumbnail = "./image/lol.jpg"
 global.donasi = "./image/donasi.jpg"
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","6285342106390","447405935355"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Alphabot-Mdོ /n' //sticker wm
-global.author = 'Created By ZeeoneOfc & Alien-Alfa' //sticker wm
+global.owner = ["919947946045"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© 𝘙𝘖𝘓𝘌𝘟-𝘔𝘋 /n' //sticker wm
+global.author = 'Created By 𝑆𝛲𝑌𝐷𝛪_.𝛫𝛪𝐷' //sticker wm
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -40,16 +40,16 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    sukses: '🤗Done, Oke Desu~',
-    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
-    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
-    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
-    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
-    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
-	bot: 'Fitur Khusus Pengguna Nomor Bot',
-    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
-    wait: '⏳ Sedang Di Proses',
-	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
+    sukses: '🤗Done~',
+    admin: 'This command can only be used by Admin !',
+    botAdmin: 'This command can only be used when the bot becomes an admin. !',
+    owner: 'This command can only be used by the owner !',
+    group: 'This command can only be used in the group !',
+    private: 'This command can only be used in private !',
+	bot: 'Special Feature User Nomor Bot',
+    errtoimg: 'Sorry, Currently Not Supporting Gif Sticker !',
+    wait: '⏳ Being proses',
+	lockCmd: 'Features Not Activated By Owner!',
 	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
 }
 
